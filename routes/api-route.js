730b25d0d890ec7b059ba03ae9d46669aa14feae
IgorFilippov3 <nostrom3@bk.ru>
2019-01-03 00:50:39 +0200

    new routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const version = require("../configs/version");
 
-const zhovtenRoute = require('./zhovten-route');
+const zhovtenRoute = require("./zhovten-route");
 // const oceanPlazaRouter = require('./ocean-plaza-router');
 const skymallRouter = require("./skymall-route");
 const kievrusRouter = require("./kievrus-route");
-const gulliverRouter = require('./gulliver-route');
+const gulliverRouter = require("./gulliver-route");
+const magelanRouter = require("./magelan-route");
+const oskarRouter = require("./oskar-dt-route");
+const leyptsigRouter = require("./leyptsig-route");
 
 router.get("/", (req, res) => res.send(`Cinema Scrapper Api ${version}`));
 
@@ -16,5 +19,8 @@ router.use("/zhovten", zhovtenRoute);
 router.use("/skymall", skymallRouter);
 router.use("/kievrus", kievrusRouter);
 router.use("/gulliver", gulliverRouter);
+router.use("/magelan", magelanRouter);
+router.use("/oskar_dt", oskarRouter);
+router.use("/leyptsig", leyptsigRouter);
 
 module.exports = router;
---
+++ routes/leyptsig-route.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const date = require('../utils/date');
+
+const vkinoListParser = require("../parsers/vkino-list-parser");
+const vkinoMovieParser = require("../parsers/vkino-movie-parser");
+
+const vkinoListInstance = new vkinoListParser("leyptsig");
+
+router.get("/", (_, res) => {
+    const today = date.todayYYYYMMDD();
+    
+    vkinoListInstance.list({date: today, lang: "ua"})
+      .then(list => {
+        const dto = JSON.stringify(list);
+        res.send(dto);
+      })
+      .catch(error => {
+        const dto = JSON.stringify(error);
+        res.send(error);
+      });
+});
+
+const vkinoMovieInstance = new vkinoMovieParser("leyptsig");
+
+router.get("/:id", (req, res) => {
+  const today = date.todayYYYYMMDD();
+
+  vkinoMovieInstance.movie({date: today, lang: "ua", id: req.params.id})
+    .then(movie => {
+      const dto = JSON.stringify(movie);
+      res.send(dto);
+    })
+    .catch(error => {
+      const dto = JSON.stringify(error);
+      res.send(dto);
+    })
+});
+
+module.exports = router;
